aec342c1d40a948f833f8c75dde8e6549459db57
Idriss Dassi Kamdoum <87118993+prims06@users.noreply.github.com>
2024-02-01 18:40:11 +0100

    delecte task implemented

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -9,6 +9,7 @@ class HomeController extends GetxController {
   final formKey = GlobalKey<FormState>();
   final editController = TextEditingController();
   final chipIndex = 0.obs;
+  final deleting = false.obs;
   final tasks = <Task>[].obs;
 
   @override
@@ -20,7 +21,12 @@ class HomeController extends GetxController {
 
   @override
   void onClose() {
+    editController.dispose();
     super.onClose();
+  }
+
+  void changeDeleting(bool value) {
+    deleting.value = value;
   }
 
   void changeChipIndex(int value) {
@@ -33,5 +39,9 @@ class HomeController extends GetxController {
     }
     tasks.add(task);
     return true;
+  }
+
+  void deleteTask(Task task) {
+    tasks.remove(task);
   }
 }
--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:whatsapp_clone/app/core/utils/extention.dart';
 import 'package:whatsapp_clone/app/core/values/colors.dart';
@@ -13,31 +14,53 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(4.0.sp),
-            child: Text(
-              'My List',
-              style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
+        body: SafeArea(
+            child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(4.0.sp),
+              child: Text(
+                'My List',
+                style:
+                    TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Obx(() => 
-             GridView.count(
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              physics: const ClampingScrollPhysics(),
-              children: [
-                TaskCard(
-                    task: Task(title: 'title', icon: 0xe59c, color: '#FF2B60E6')),
-                ...controller.tasks.map((task)=>TaskCard(task: task)).toList(),
-                AddCard()
-              ],
-            ),
-          )
-        ],
-      )),
-    );
+            Obx(
+              () => GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                children: [
+                  TaskCard(
+                      task: Task(
+                          title: 'title', icon: 0xe59c, color: '#FF2B60E6')),
+                  ...controller.tasks
+                      .map((task) => LongPressDraggable(
+                          data: task,
+                          feedback: Opacity(opacity: 0.8),
+                          onDragStarted: () => controller.changeDeleting(true),
+                          onDraggableCanceled: (_, __) =>
+                              controller.changeDeleting(false),
+                          onDragEnd: (_) => controller.changeDeleting(false),
+                          child: TaskCard(task: task)))
+                      .toList(),
+                  AddCard()
+                ],
+              ),
+            )
+          ],
+        )),
+        floatingActionButton: DragTarget<Task>(
+          builder: (_, __, ___) => Obx((() => FloatingActionButton(
+                backgroundColor: controller.deleting.value ? Colors.red : blue,
+                onPressed: () {},
+                child:
+                    Icon(controller.deleting.value ? Icons.add : Icons.delete),
+              ))),
+          onAccept: (task) {
+            controller.deleteTask(task);
+            EasyLoading.showSuccess('Task delected');
+          },
+        ));
   }
 }
